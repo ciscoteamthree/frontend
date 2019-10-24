ba2a666cea1d2c0c5ad43075192e84ad83aa5acb
Orhan Hirsch <orhanhenrik@gmail.com>
2019-10-24 21:58:07 +0200

    cleanup

--- components/WebexClient.js
+++ components/WebexClient.js
@@ -1,30 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const accessToken = '';
 const spaceId3 =
   'Y2lzY29zcGFyazovL2h0dHBzOi8vY29udi1hLndieDIuY29tL2NvbnZlcnNhdGlvbi9hcGkvdjEvY29udmVyc2F0aW9ucy82Y2M0NDA3MC1mNTE1LTExZTktOTE1NC01NWQwYjgxZGMxZjAvUk9PTS82Y2M0NDA3MC1mNTE1LTExZTktOTE1NC01NWQwYjgxZGMxZjA';
 const spaceTredjePlass =
   'Y2lzY29zcGFyazovL2h0dHBzOi8vY29udi1hLndieDIuY29tL2NvbnZlcnNhdGlvbi9hcGkvdjEvY29udmVyc2F0aW9ucy8xMWVmYTY5MC1kZmQ0LTExZTktYTViMy02MzI4YTk1NDFjMTkvUk9PTS8xMWVmYTY5MC1kZmQ0LTExZTktYTViMy02MzI4YTk1NDFjMTk';
-const spaceWidgetProps = {
-  accessToken: accessToken,
-  destinationType: 'spaceId',
-  destinationId:
-    'Y2lzY29zcGFyazovL2h0dHBzOi8vY29udi1hLndieDIuY29tL2NvbnZlcnNhdGlvbi9hcGkvdjEvY29udmVyc2F0aW9ucy82Y2M0NDA3MC1mNTE1LTExZTktOTE1NC01NWQwYjgxZGMxZjAvUk9PTS82Y2M0NDA3MC1mNTE1LTExZTktOTE1NC01NWQwYjgxZGMxZjA'
-};
 
-const recentsWidgetProps = {
-  accessToken: accessToken
-};
-
-const WebexClient = props => {
-  const [token, setToken] = useState('https://e479bf03.ngrok.io');
-
-  const setRoom = (name, detail) => {
-    console.log(name);
-    console.log(detail);
-    //setSpaceId(roomId);
-  };
+const WebexClient = () => {
+  const [token, setToken] = useState();
 
   useEffect(() => {
     const socket = io('https://e479bf03.ngrok.io');
