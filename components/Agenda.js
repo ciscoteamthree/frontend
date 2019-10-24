9104635b8f1b685dafade013f3f2c994dea0534d
Peder Smith <smith.peder@gmail.com>
2019-10-24 20:52:43 +0200

    New agenda

--- components/Agenda.js
+++ components/Agenda.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import Flex from 'styled-flex-component';
 import moment from 'moment';
 import { SortablePane, Pane } from 'react-sortable-pane';
+
+const PaneWrapper = styled(Pane)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: solid 1px #ddd;
+  background: ${props => props.background};
+`;
+
+const TextStyle = styled.h3`
+  font-size: 16px;
+  font-weight: bold;
+`;
 
 const Agenda = props => {
   const { agenda } = props;
   const slices =
     agenda &&
-    agenda.map(slice => (
-      <Pane key={slice.id} defaultSize={{ width: '100%', height: 120 }}>
-        <h2>{slice.title}</h2>
-        <h2>{slice.description}</h2>
-        <h2>{slice.duration}</h2>
-        <h2>{slice.color}</h2>
-      </Pane>
-    ));
+    agenda.map(slice => {
+      console.log(slice);
+      return (
+        <PaneWrapper
+          key={slice.title}
+          defaultSize={{ width: '100%', height: 120 }}
+          style={{ background: slice.color }}
+        >
+          <TextStyle key={slice.id}>{slice.title}</TextStyle>
+        </PaneWrapper>
+      );
+    });
   return (
     <div style={{ padding: '10px' }}>
       <SortablePane direction="vertical" margin={20}>
