c17c0388e7afea9cf3fe2ecbc19c5f17ca172f3b
Ludvig Hozman <ludvig123@gmail.com>
2019-10-24 20:40:20 +0200

    Use ws for token

--- components/WebexClient.js
+++ components/WebexClient.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
-const accessToken =
-  '';
+const accessToken = '';
 const spaceId3 =
   'Y2lzY29zcGFyazovL2h0dHBzOi8vY29udi1hLndieDIuY29tL2NvbnZlcnNhdGlvbi9hcGkvdjEvY29udmVyc2F0aW9ucy82Y2M0NDA3MC1mNTE1LTExZTktOTE1NC01NWQwYjgxZGMxZjAvUk9PTS82Y2M0NDA3MC1mNTE1LTExZTktOTE1NC01NWQwYjgxZGMxZjA';
+const spaceTredjePlass =
+  'Y2lzY29zcGFyazovL2h0dHBzOi8vY29udi1hLndieDIuY29tL2NvbnZlcnNhdGlvbi9hcGkvdjEvY29udmVyc2F0aW9ucy8xMWVmYTY5MC1kZmQ0LTExZTktYTViMy02MzI4YTk1NDFjMTkvUk9PTS8xMWVmYTY5MC1kZmQ0LTExZTktYTViMy02MzI4YTk1NDFjMTk';
 const spaceWidgetProps = {
   accessToken: accessToken,
   destinationType: 'spaceId',
@@ -18,6 +20,7 @@ const recentsWidgetProps = {
 const WebexClient = props => {
   const [spaceId, setSpaceId] = useState(null);
   const [ready, setReady] = useState(false);
+  const [token, setToken] = useState('https://e479bf03.ngrok.io');
 
   const setRoom = (name, detail) => {
     console.log(name);
@@ -26,15 +29,27 @@ const WebexClient = props => {
   };
 
   useEffect(() => {
+    const socket = io('https://e479bf03.ngrok.io');
+    socket.on('token', token => {
+      console.log('token', token);
+      setToken(token);
+    });
+  }, []);
+
+  useEffect(() => {
     const spaceWidget = document.getElementById('webex-widget');
     const recentsWidget = document.getElementById('webex-recents');
     console.log(ready);
     //if (spaceWidget && spaceId && !ready) {
-    ciscospark.widget(spaceWidget).spaceWidget({
-      accessToken: accessToken,
-      destinationType: 'spaceId',
-      destinationId: spaceId3
-    });
+    if (token) {
+      ciscospark.widget(spaceWidget).spaceWidget({
+        accessToken: token,
+        destinationType: 'spaceId',
+        destinationId: spaceTredjePlass,
+        initialActivity: 'meet',
+        startCall: true
+      });
+    }
     //setReady(true);
     //} else if (recentsWidget && !ready) {
     //ciscospark.widget(recentsWidget).recentsWidget({
@@ -42,7 +57,7 @@ const WebexClient = props => {
     //onEvent: setRoom
     //});
     //}
-  });
+  }, [spaceId, token]);
   return (
     <div>
       {
