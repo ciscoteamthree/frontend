143f6534f2702535aeca932dc475ccf7c1acec12
Ludvig Hozman <ludvig123@gmail.com>
2019-10-25 09:53:31 +0200

    Don't start meeting when there is no registered meeting

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import WebexClient from '../components/WebexClient';
+import MeetingWait from '../components/MeetingWait';
+import Agenda from '../components/Agenda';
+import io from 'socket.io-client';
+import moment from 'moment';
 
-const Home = () => (
-  <div
-    style={{
-      display: 'grid',
-      gridTemplateColumns: '8fr 2fr',
-      gridTemplateRows: '1fr',
-      gridColumnGap: '5px',
-      height: '800px'
-    }}
-  >
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <WebexClient />
-    <div>Placeholder</div>
-  </div>
-);
+const Client = () => {
+  const [currentMeeting, setCurrentMeeting] = useState();
 
-export default Home;
+  useEffect(() => {
+    const socket = io('https://2a9d7c02.ngrok.io');
+    socket.on('currentMeeting', meeting => {
+      setCurrentMeeting(meeting);
+    });
+  });
+
+  const meetingReady = meeting => {
+    return meeting && moment.now().after(meeting.startTime);
+  };
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '8fr 2fr',
+        gridTemplateRows: '1fr',
+        gridColumnGap: '5px',
+        height: '100vh'
+      }}
+    >
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {meetingReady(currentMeeting) ? (
+        <WebexClient />
+      ) : (
+        <MeetingWait meeting={currentMeeting} />
+      )}{' '}
+      <div>
+        <Agenda agenda={currentMeeting && currentMeeting.agenda} />
+      </div>
+    </div>
+  );
+};
+
+export default Client;
