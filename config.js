a128b73a949872c75965376e0ae52f687dfbfe14
Peder Smith <smith.peder@gmail.com>
2019-10-26 10:39:47 +0200

    NGROK

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const WS_URL = 'https://ab27b546.ngrok.io';
+export const WS_URL = 'https://534f7ff7.ngrok.io';
 //export const WS_URL = 'http://localhost:8000';
 //export const WS_URL = 'http://172.17.4.120:8000';
 
